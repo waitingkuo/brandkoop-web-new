763e78a0b36b3eb060e41e164096b92731397301
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-07-28 23:34:54 +0800

    add example in the index page

--- app/client/views/index/index.es6.js
+++ app/client/views/index/index.es6.js
@@ -1,5 +1,40 @@
 Meteor.startup(function() {
 
+  Template.index.helpers({
+  
+    slack: {
+      name: 'Slack',
+      img: '/images/index/slack.png',
+      valuesChartImg: '/images/index/slack-values.png'
+    }, 
+
+    blueBottle: {
+      name: 'Blue Bottle',
+      img: '/images/index/blue-bottle.png',
+      valuesChartImg: '/images/index/slack-values.png'
+    }, 
+
+    honest: {
+      name: 'Honest',
+      img: '/images/index/honest.jpeg',
+      valuesChartImg: '/images/index/slack-values.png'
+    },
+
+    hillaryClinton: {
+      name: 'Hillary Clinton',
+      img: '/images/index/hillary-clinton.jpeg',
+      valuesChartImg: '/images/index/slack-values.png'
+    },
+
+    redBull: {
+      name: 'Red Bull',
+      img: '/images/index/red-bull.png',
+      valuesChartImg: '/images/index/slack-values.png'
+    }
+
+
+  });
+
 
 });
 
