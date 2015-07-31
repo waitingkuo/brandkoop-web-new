15eabe36dbd9814a6ae8c1dde160249f5708d77d
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-07-31 22:11:50 +0800

    add see profile

--- app/client/views/index/index.es6.js
+++ app/client/views/index/index.es6.js
@@ -5,33 +5,56 @@ Meteor.startup(function() {
     slack: {
       name: 'Slack',
       img: '/images/index/slack.png',
-      valuesChartImg: '/images/index/slack-values.png'
+      valuesChartImg: '/images/index/slack-values.png',
+      domain: 'slack.com'
     },
 
     blueBottle: {
       name: 'Blue Bottle',
       img: '/images/index/blue-bottle.png',
-      valuesChartImg: '/images/index/blue-bottle-values.png'
+      valuesChartImg: '/images/index/blue-bottle-values.png',
+      domain: 'bluebottlecoffee.com'
     },
 
     honest: {
       name: 'Honest',
       img: '/images/index/honest.jpeg',
-      valuesChartImg: '/images/index/honest-values.png'
+      valuesChartImg: '/images/index/honest-values.png',
+      domain: 'www.honest.com'
     },
 
     hillaryClinton: {
       name: 'Hillary Clinton',
       img: '/images/index/hillary-clinton.jpeg',
-      valuesChartImg: '/images/index/hillary-clinton-values.png'
+      valuesChartImg: '/images/index/hillary-clinton-values.png',
+      domain: 'www.hillaryclinton.com'
     },
 
     redBull: {
       name: 'Red Bull',
       img: '/images/index/red-bull.png',
-      valuesChartImg: '/images/index/red-bull-values.png'
+      valuesChartImg: '/images/index/red-bull-values.png',
+      domain: 'www.redbull.com'
     }
 
+
+  });
+
+  Template.exampleProfile.events({
+
+    'click .see-profile': function(event) {
+
+      event.preventDefault();
+
+      // Hack FIXME
+      Meteor.call('insertInstantProfile', this.domain, function(err, result) {
+        if (!err) {
+          let id = result;
+          FlowRouter.go('/instantProfiler/result/'+id);
+        }
+      });
+
+    }
 
   });
 
