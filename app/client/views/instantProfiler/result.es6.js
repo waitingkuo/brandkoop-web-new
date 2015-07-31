909d4a698baabdd7dcf8a28c2ffab33c67494c34
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-07-31 21:29:14 +0800

    text for twitter button

--- app/client/views/instantProfiler/result.es6.js
+++ app/client/views/instantProfiler/result.es6.js
@@ -18,6 +18,7 @@ Meteor.startup(function() {
 
   });
 
+
   Template.instantProfilerResultProfiled.helpers({
 
     profile() {
@@ -29,13 +30,71 @@ Meteor.startup(function() {
       let profileId = FlowRouter.getParam('instantProfileId');
       let profile = InstantProfiles.findOne(profileId);
       let domain = profile.domain
-      let topValues = 'Friendly, Friendly, Friendly, Friendly, Friendly'
 
-      let text = domain + "'s personality is " + topValues + ' bit.ly/xxxxxxx' + ' Find out yours @thebrandkoop';
-      console.log(text);
+      let topValueObjects = _.sortBy(profile.values, 'frequency').reverse().slice(0, 5);
+      topValues = _.map(topValueObjects, function(o){
+        return o.trait;
+      });
 
+      let limit = 140;
+
+      // whole sentence
+      // "www.xxx.com's personality is c1, c2, c3, c4, c5. 
+      //   http://www.brandkoop.com/xxxxx
+      //   via @thebrandKoop
+
+      // "www.xxx.com" url's length is 22 in twitter
+      limit -= 22
+
+      let personality = "'s personality is ";
+      limit -= personality.length;
+
+      // link in twitter is 22 ch length
+      //+1 for prefix space
+      //let link = ' http://www.brandkoop.com/xxxxxxx';
+      let link = ' bit.ly/xxxxxxx'
+      limit -= 22 + 1;
+
+      //' Find out yours @thebrandkoop';
+      //let ending = ' Find out yours';
+      //limit -= ending.length;
+      
+      // via @thebrandKoop  (automatically generated)
+      limit -= ' via @thebrandKoop'.length;
+      
+      let isFirst = true;
+      let values = ''
+      for (let value of topValues) {
+        if (isFirst) {
+          // consider space
+          if (limit >= value.length + 1) {
+            values += value + ' ';
+            isFirst = false;
+            limit -= value.length + 1;
+          }
+        } else {
+          if (limit >= value.length + 2) {
+            values += ',' + value + ' ';
+            limit -= value.length + 2;
+          }
+        }
+      }
+
+
+      //let text = domain + personality + values + link + ending;
+      let text = domain + personality + values + link;
+
+      
       return text;
     }
+
+  });
+
+  Template.instantProfilerResultProfiled.onDestroyed(function() {
+
+    // remove this (a script) so 
+    // next time twitter button can show correctly
+    $('#twitter-wjs').remove();
 
   });
 
