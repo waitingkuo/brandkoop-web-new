a12e55bb13f037e0b699cd1c864f8533e4ae697d
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-08-06 16:17:20 +0800

    update story

--- app/client/lib/charts/liteProfilerStoryies.es6.js
+++ app/client/lib/charts/liteProfilerStoryies.es6.js
@@ -9,40 +9,41 @@ LiteProfilerDescriptions.characters = {
 };
 
 LiteProfilerDescriptions.values = {
-"friendly": "Just like Mailchimp (mailchimp.com)"
-"down-to-earth": "An example: Juan Valdez (juanvaldezcafe.com)"
-"down to earth": "An example: Juan Valdez (juanvaldezcafe.com)"
-"cheerful": "Mc Donald's brings cheer (mcdonalds.com)"
-"honest": "Honesty=Starbucks (starbucks.com)"
-"original": "Just like Coke  (coca-cola.com)"
-"real": "Them real potatoes: Kettle (kettlebrand.com)"
-"sentimental": "Tiffany is sentimental (tiffany.com)"
-"wholesome": "It's in the name: Wholefoods (wholefoods.com)"
-"family-oriented": "Sweet family by Honeymaid (honeymaid.com)"
-"daring": "A daring brand: Monster Energy (monsterenergy.com)"
-"exciting": "Exciting=Red Bull (redbull.com)"
-"imaginative": "A fine example: Apple (apple.com)"
-"independent": "You're independent like Converse (converse.com)"
-"spirited": "You have spirit like Diesel (diesel.com)"
-"trendy": "Zara is trendy like you (zara.com)"
-"unique": "The one and only, Beetle (vw.com/models/beetle)"
-"up-to-date": "Samsung makes you up-to-date (samsung.com)"
-"young": "Just like Doritos  (doritos.com)"
-"confident": "For example: Gillette (gillette.com)"
-"reliable": "On time with UPS (ups.com)"
-"corporate": "Suit up with IBM (ibm.com)"
-"hard working": "Mr. Clean knows hard work (mrclean.com)"
-"intelligent": "You're a smart cookie like 3M (3m.com)"
-"technical": "Just like Google (google.com)"
-"successful": "You promote success just like Nike (nike.com)"
-"secure": "Just like All State (allstate.com)"
-"charming": "You turn on the charm like Hilton (hilton.com)"
-"feminine": "A fine example: Victoria's Secret (victoriassecret.com)"
-"glamorous": "You display glamour like Coco Chanel (chanel.com)"
-"good-looking": "Tommy knows good looks (tommy.com)"
-"upper-class": "An example: Cartier (cartier.com)"
-"masculine": "Just like Harley Davidson (harley-davidson.com)"
-"outdoorsy": "Explore outside like Patagonia (patagonia.com)"
-"tough": "You're tough like Caterpillar (cat.com)"
-"rugged": "Rugged, as in Jeep (jeep.com)"
+"friendly": "Just like Mailchimp (mailchimp.com)",
+"down-to-earth": "An example: Juan Valdez (juanvaldezcafe.com)",
+"down to earth": "An example: Juan Valdez (juanvaldezcafe.com)",
+"cheerful": "Mc Donald's brings cheer (mcdonalds.com)",
+"honest": "Honesty=Starbucks (starbucks.com)",
+"original": "Just like Coke  (coca-cola.com)",
+"real": "Them real potatoes: Kettle (kettlebrand.com)",
+"sentimental": "Tiffany is sentimental (tiffany.com)",
+"wholesome": "It's in the name: Wholefoods (wholefoods.com)",
+"family oriented": "Sweet family by Honeymaid (honeymaid.com)",
+"family-oriented": "Sweet family by Honeymaid (honeymaid.com)",
+"daring": "A daring brand: Monster Energy (monsterenergy.com)",
+"exciting": "Exciting=Red Bull (redbull.com)",
+"imaginative": "A fine example: Apple (apple.com)",
+"independent": "You're independent like Converse (converse.com)",
+"spirited": "You have spirit like Diesel (diesel.com)",
+"trendy": "Zara is trendy like you (zara.com)",
+"unique": "The one and only, Beetle (vw.com/models/beetle)",
+"up-to-date": "Samsung makes you up-to-date (samsung.com)",
+"young": "Just like Doritos  (doritos.com)",
+"confident": "For example: Gillette (gillette.com)",
+"reliable": "On time with UPS (ups.com)",
+"corporate": "Suit up with IBM (ibm.com)",
+"hard working": "Mr. Clean knows hard work (mrclean.com)",
+"intelligent": "You're a smart cookie like 3M (3m.com)",
+"technical": "Just like Google (google.com)",
+"successful": "You promote success just like Nike (nike.com)",
+"secure": "Just like All State (allstate.com)",
+"charming": "You turn on the charm like Hilton (hilton.com)",
+"feminine": "A fine example: Victoria's Secret (victoriassecret.com)",
+"glamorous": "You display glamour like Coco Chanel (chanel.com)",
+"good-looking": "Tommy knows good looks (tommy.com)",
+"upper-class": "An example: Cartier (cartier.com)",
+"masculine": "Just like Harley Davidson (harley-davidson.com)",
+"outdoorsy": "Explore outside like Patagonia (patagonia.com)",
+"tough": "You're tough like Caterpillar (cat.com)",
+"rugged": "Rugged, as in Jeep (jeep.com)",
 };
